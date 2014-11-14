3e1a91a092a44167d1954a4c2c9d7679728a5d4a
root <root@judge.koreatech.ac.kr>
2014-11-14 23:54:00 +0900

    first commit

---
+++ template/koreatech/oj-footer.php
@@ -0,0 +1,13 @@
+<span class=center>
+
+	<?php if(file_exists("setlang.php")){?>
+		<a href=setlang.php?lang=ko>한국어</a>&nbsp;
+		<a href=setlang.php?lang=en>English</a><br>
+	<?php }?>
+
+
+		<br>
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2014 <a href='https://github.com/zhblue/hustoj'>HUSTOJ Project</a> TEAM<br>
+    &copy; <a href='https://github.com/koreatech/JudgeOnline'>Koreatech OnlineJudge</a><br>
+
+</span>
