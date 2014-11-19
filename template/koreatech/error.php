77652673594905cb9e0580a6fb05b34e8c78337c
Mastojun <mastojun@gmail.com>
2014-11-19 13:20:40 +0900

    contest 의 에러페이지에서는 상단 navigation이 contest용으로

--- template/koreatech/error.php
+++ template/koreatech/error.php
@@ -6,9 +6,15 @@
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
-<?php require_once("oj-header.php");?>
+<?php
+  if(isset($_GET['cid'])) {
+    require_once("./template/$OJ_TEMPLATE/contest-header.php");
+  } else {
+    require_once("oj-header.php");
+  }
+?>
 <div class="container">
-	<?php echo $view_errors?>
+  <?php echo $view_errors?>
 </div>
 <?php require_once("oj-footer.php");?>
 <?php require_once("include-bottom.php");?>
