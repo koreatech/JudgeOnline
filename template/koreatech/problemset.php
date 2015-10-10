7eb11c7e61080055c48355f6addc8076a4fd91bd
Mastojun <mastojun@gmail.com>
2015-10-11 02:07:26 +0900

    문제 리스트 페이지 수정
    * 코드 들여쓰기 수정
    * 문제번호 찾는 form제거
    * 검색 form 수정
    * 문제 ID의 a tag 제거

--- template/koreatech/problemset.php
+++ template/koreatech/problemset.php
@@ -1,80 +1,71 @@
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title><?php echo $view_title?></title>
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
 <?php
-  $navigation_tab = "problem";
-  require_once("oj-header.php");
+$navigation_tab = "problem";
+require_once("oj-header.php");
 ?>
 
 <div class="container">
 <nav class="text-center">
   <ul class="pagination pagination-sm">
 <?php
-    for ($i=1;$i<=$view_total_page;$i++){
-      if ($i==$page) echo "<li class='active'><span>".$i."<span class='sr-only'>(current)</span></span></li>";
-      else echo "<li><a href='problemset.php?page=".$i."'>".$i."</a></li>";
-    }
+for ($i=1;$i<=$view_total_page;$i++){
+  if ($i==$page) echo "<li class='active'><span>".$i."<span class='sr-only'>(current)</span></span></li>";
+  else echo "<li><a href='problemset.php?page=".$i."'>".$i."</a></li>";
+}
 ?>
   </ul>
 </nav>
 
-  <table>
-  <tr align='center' class='evenrow'><td width='5'></td>
-			<td width='50%' colspan='1'>
-				<form class=form-search action=problem.php>
-					Problem ID<input class="input-small search-query" type='text' name='id' size=5 style="height:24px">
-                                  <button class="btn btn-mini" type='submit'  >Go</button></form>
-			</td>
-			<td width='50%' colspan='1'>
-			<form class="form-search">
-				<input style="height:24px" type="text" name=search class="input-large search-query">
-				<button type="submit" class="btn btn-mini"><?php echo $MSG_SEARCH?></button>
-			</form>
-			</td></tr>
+  <div class="col-md-12 text-right">
+    <form class="form-inline">
+      <div class="form-group">
+        <label class="sr-only" for="searcheTerm">search term</label>
+        <input class="form-control" type="text" name="search" id="searchTerm">
+      </div>
+      <button class="btn btn-mini" type="submit"><?php echo $MSG_SEARCH?></button>
+    </form>
+  </div>
+
+  <table id='problemset' class='table table-striped'>
+    <thead>
+      <tr class='toprow'>
+        <th width='5'></th>
+        <th width='120'><?php echo $MSG_PROBLEM_ID?></th>
+        <th><?php echo $MSG_TITLE?></th>
+        <th width='10%'><?php echo $MSG_SOURCE?></th>
+        <th style="cursor:hand"  width=60 ><?php echo $MSG_AC?></th>
+        <th style="cursor:hand" width=60 ><?php echo $MSG_SUBMIT?></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php 
+$cnt=0;
+foreach($view_problemset as $row){
+  if ($cnt) 
+    echo "<tr class='oddrow'>";
+  else
+    echo "<tr class='evenrow'>";
+  foreach($row as $table_cell){
+    echo "<td>";
+    echo "\t".$table_cell;
+    echo "</td>";
+  }
+
+  echo "</tr>";
+
+  $cnt=1-$cnt;
+}
+?>
+    </tbody>
   </table>
-
-	<table id='problemset' class='table table-striped'>
-                <thead>
-
-                          <tr class='toprow'>
-                            <th width='5'></th>
-                          	<th width='120'  ><A><?php echo $MSG_PROBLEM_ID?></A></th>
-                            <th><?php echo $MSG_TITLE?></th>
-                            <th width='10%'><?php echo $MSG_SOURCE?></th>
-                            <th style="cursor:hand"  width=60 ><?php echo $MSG_AC?></th>
-                            <th style="cursor:hand" width=60 ><?php echo $MSG_SUBMIT?></th>
-                          </tr>
-                </thead>
-
-		  
-			<tbody>
-			<?php 
-			$cnt=0;
-			foreach($view_problemset as $row){
-				if ($cnt) 
-					echo "<tr class='oddrow'>";
-				else
-					echo "<tr class='evenrow'>";
-				foreach($row as $table_cell){
-					echo "<td>";
-					echo "\t".$table_cell;
-					echo "</td>";
-				}
-				
-				echo "</tr>";
-				
-				$cnt=1-$cnt;
-			}
-			?>
-			</tbody>
-			</table>
 </div>
 <?php require_once("oj-footer.php");?>
-
-	<?php require_once("include-bottom.php");?>
+<?php require_once("include-bottom.php");?>
 </body>
 </html>
