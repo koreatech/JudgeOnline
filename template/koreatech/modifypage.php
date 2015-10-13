8c22e9f476c7cf17d13a47d2d3693c1ba5bba43a
Mastojun <mastojun@gmail.com>
2015-10-13 15:49:43 +0900

    들여쓰기 변경

--- template/koreatech/modifypage.php
+++ template/koreatech/modifypage.php
@@ -1,55 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title><?php echo $view_title?></title>
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
 <div id="wrapper">
-	<?php require_once("oj-header.php");?>
+  <?php require_once("oj-header.php");?>
 <div id=main>
-	<form action="modify.php" method="post">
-	<br><br>
-	<center><table>
-		<tr><td colspan=2 height=40 width=500>&nbsp;&nbsp;&nbsp;Update Information</tr>
-		<tr><td width=25%>User ID:
-			<td width=75%><?php echo $_SESSION['user_id']?>
-			<?php require_once('./include/set_post_key.php');?>
-		</tr>
-		<tr><td>Nick Name:
-			<td><input name="nick" size=50 type=text value="<?php echo htmlspecialchars($row->nick)?>" >
-		</tr>
-		<tr><td>Old Password:
-			<td><input name="opassword" size=20 type=password>
-		</tr>
-		<tr><td>New Password:
-			<td><input name="npassword" size=20 type=password>
-		</tr>
-		<tr><td>Repeat New Password::
-			<td><input name="rptpassword" size=20 type=password>
-		</tr>
-		<tr><td>School:
-			<td><input name="school" size=30 type=text value="<?php echo htmlspecialchars($row->school)?>" >
-		</tr>
-		<tr><td>Email:
-			<td><input name="email" size=30 type=text value="<?php echo htmlspecialchars($row->email)?>" >
-		</tr>
-		<tr><td>
-			<td><input value="Submit" name="submit" type="submit">
-				&nbsp; &nbsp;
-				<input value="Reset" name="reset" type="reset">
-		</tr>
-	</table></center>
-	<br>
-	<a href=export_ac_code.php>Download All AC Source</a>
-	<br>
+  <form action="modify.php" method="post">
+  <br><br>
+  <center><table>
+    <tr><td colspan=2 height=40 width=500>&nbsp;&nbsp;&nbsp;Update Information</tr>
+    <tr><td width=25%>User ID:
+      <td width=75%><?php echo $_SESSION['user_id']?>
+      <?php require_once('./include/set_post_key.php');?>
+    </tr>
+    <tr><td>Nick Name:
+      <td><input name="nick" size=50 type=text value="<?php echo htmlspecialchars($row->nick)?>" >
+    </tr>
+    <tr><td>Old Password:
+      <td><input name="opassword" size=20 type=password>
+    </tr>
+    <tr><td>New Password:
+      <td><input name="npassword" size=20 type=password>
+    </tr>
+    <tr><td>Repeat New Password::
+      <td><input name="rptpassword" size=20 type=password>
+    </tr>
+    <tr><td>School:
+      <td><input name="school" size=30 type=text value="<?php echo htmlspecialchars($row->school)?>" >
+    </tr>
+    <tr><td>Email:
+      <td><input name="email" size=30 type=text value="<?php echo htmlspecialchars($row->email)?>" >
+    </tr>
+    <tr><td>
+      <td><input value="Submit" name="submit" type="submit">
+        &nbsp; &nbsp;
+        <input value="Reset" name="reset" type="reset">
+    </tr>
+  </table></center>
+  <br>
+  <a href=export_ac_code.php>Download All AC Source</a>
+  <br>
 <div id=foot>
-	<?php require_once("oj-footer.php");?>
+  <?php require_once("oj-footer.php");?>
 
 </div><!--end foot-->
 </div><!--end main-->
 </div><!--end wrapper-->
-	<?php require_once("include-bottom.php");?>
+  <?php require_once("include-bottom.php");?>
 </body>
 </html>
