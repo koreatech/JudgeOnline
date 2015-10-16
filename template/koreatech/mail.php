d295211ad41cec154f60de1179e01fea21c71470
Mastojun <mastojun@gmail.com>
2015-10-16 13:01:56 +0900

    mail.php 수정
    * dom구조 변경
    * bootstrap 적용

--- template/koreatech/mail.php
+++ template/koreatech/mail.php
@@ -1,70 +1,81 @@
 <!DOCTYPE html>
-<html>
+<html lang="ko">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <title><?php echo $view_title?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
-<div id="wrapper">
-	<?php require_once("oj-header.php");?>
-<div id=main>
-	<center>
-	<?php
-	if($view_content)
-	echo "<center>
-	<table>
-			<tr>
-				<td class=blue>$to_user:".htmlspecialchars(str_replace("\n\r","\n",$view_title))." </td>
-			</tr>
-			<tr><td><pre>". htmlspecialchars(str_replace("\n\r","\n",$view_content))."</pre>		
-				</td></tr>
-    </table></center>";
-	
-	?>
-   <table><form method=post action=mail.php>
-	<tr>
-		<td>  To:<input name=to_user size=10 value="<?php echo $to_user?>">
-			Title:<input name=title size=20 value="<?php echo $title?>">
-		    <input type=submit value=<?php echo $MSG_SUBMIT?>></td>
-	</tr>
-	<tr><td> 
-		<textarea name=content rows=10 cols=80 class="input input-xxlarge"></textarea>
-	  
-	 </td></tr>
-	</form>
-   </table>
-   <table border=1>
-   <tr><td>Mail ID<td>From:Title<td>Date</tr>
-   <tbody>
-			<?php 
-			$cnt=0;
-			foreach($view_mail as $row){
-				if ($cnt) 
-					echo "<tr class='oddrow'>";
-				else
-					echo "<tr class='evenrow'>";
-				foreach($row as $table_cell){
-					echo "<td>";
-					echo "\t".$table_cell;
-					echo "</td>";
-				}
-				
-				echo "</tr>";
-				
-				$cnt=1-$cnt;
-			}
-			?>
-			</tbody>
-	</table>
-</center> 
-	 
-<div id=foot>
-	<?php require_once("oj-footer.php");?>
-
-</div><!--end foot-->
-</div><!--end main-->
-</div><!--end wrapper-->
-	<?php require_once("include-bottom.php");?>
+<?php require_once("oj-header.php");?>
+<div class="container">
+  <form class="form-horizontal" method="post" action="mail.php">
+<?php
+if($view_content) {
+?>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <h3>
+          <?php echo htmlspecialchars(str_replace("\n\r","\n",$view_title));?> <small>from <strong><?php echo $to_user?></strong></small>
+        </h3>
+      </div>
+      <div class="col-sm-offset-2 col-sm-10">
+        <pre><?php echo htmlspecialchars(str_replace("\n\r","\n",$view_content));?></pre>
+      </div>
+    </div>
+<?php
+}
+?>
+    <div class="form-group">
+      <label for="inputToUser" class="col-sm-2 control-label">받는사람</label>
+      <div class="col-sm-10">
+        <input id="inputToUser" name="to_user" class="form-control" value="<?php echo $to_user?>">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputTitle" class="col-sm-2 control-label">제목</label>
+      <div class="col-sm-10">
+        <input id="inputTitle" name="title" class="form-control" value="<?php echo $title?>">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="textareaContent" class="col-sm-2 control-label">내용</label>
+      <div class="col-sm-10">
+        <textarea name="content" rows="10" class="form-control"></textarea>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-2">
+        <button type="submit" class="btn btn-primary">보내기</button>
+      </div>
+    </div>
+  </form>
+  <div class="container">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>보낸사람</td>
+          <td>제목</td>
+          <td>날짜</td>
+        </tr>
+      </thead>
+      <tbody>
+<?php 
+foreach($view_mail as $row){
+  echo "<tr>";
+  foreach($row as $table_cell){
+    echo "<td>";
+    echo $table_cell;
+    echo "</td>";
+  }
+  echo "</tr>";
+}
+?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<?php require_once("oj-footer.php");?>
+<?php require_once("include-bottom.php");?>
 </body>
 </html>
