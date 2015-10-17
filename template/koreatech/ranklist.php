714ccf557f2e6d533fb35c2f0c8e9cc204112fd3
Mastojun <mastojun@gmail.com>
2015-10-17 21:23:38 +0900

    ranklist.php 페이지 수정
    * bootstrap 적용
    * table dom 구조 변경 (form 과 table 분리)

--- template/koreatech/ranklist.php
+++ template/koreatech/ranklist.php
@@ -1,73 +1,72 @@
 <!DOCTYPE html>
-<html>
+<html lang="ko">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <title><?php echo $view_title?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
 <?php
-  $navigation_tab = "ranklist";
-  require_once("oj-header.php");
+$navigation_tab = "ranklist";
+require_once("oj-header.php");
 ?>
 
 <div class="container">
-	<table class="table text-center">
-		<thead>
-		<tr><td colspan=3 align=left>
-			<form action=userinfo.php>
-				<?php echo $MSG_USER?><input name=user>
-				<input type=submit value=Go>
-			</form></td><td colspan=3 align=right>
-			<a href=ranklist.php?scope=d>Day</a>
-			<a href=ranklist.php?scope=w>Week</a>
-			<a href=ranklist.php?scope=m>Month</a>
-			<a href=ranklist.php?scope=y>Year</a>
-			</td></tr>
-		<tr class='toprow'>
-				<td width=5% align=center><b><?php echo $MSG_Number?></b>
-				<td width=10% align=center><b><?php echo $MSG_USER?></b>
-				<td width=55% align=center><b><?php echo $MSG_NICK?></b>
-				<td width=10% align=center><b><?php echo $MSG_AC?></b>
-				<td width=10% align=center><b><?php echo $MSG_SUBMIT?></b>
-				<td width=10% align=center><b><?php echo $MSG_RATIO?></b>
-		</tr>
-		</thead>
-		<tbody>
-			<?php 
-			$cnt=0;
-			foreach($view_rank as $row){
-				if ($cnt) 
-					echo "<tr class='oddrow'>";
-				else
-					echo "<tr class='evenrow'>";
-				foreach($row as $table_cell){
-					echo "<td>";
-					echo "\t".$table_cell;
-					echo "</td>";
-				}
-				
-				echo "</tr>";
-				
-				$cnt=1-$cnt;
-			}
-			?>
-			</tbody>		
-	</table>
-	<?php 
-	   echo "<center>";
-		for($i = 0; $i <$view_total ; $i += $page_size) {
-			echo "<a href='./ranklist.php?start=" . strval ( $i ).($scope?"&scope=$scope":"") . "'>";
-			echo strval ( $i + 1 );
-			echo "-";
-			echo strval ( $i + $page_size );
-			echo "</a>&nbsp;";
-			if ($i % 250 == 200)
-				echo "<br>";
-		}
-		echo "</center>";
-	
-	?>
+  <div class="col-sm-6 text-left">
+    <form action="userinfo.php" class="form-inline">
+      <div class="form-group">
+        <label class="control=label" for="searchUser"><?php echo $MSG_USER?></label>
+        <input class="form-control" type="text" name="user">
+        <button class="btn btn-mini" type="submit">Go</button>
+      </div>
+    </form>
+  </div>
+  <div class="col-sm-6 text-right">
+    <a href="ranklist.php?scope=d">Day</a>
+    <a href="ranklist.php?scope=w">Week</a>
+    <a href="ranklist.php?scope=m">Month</a>
+    <a href="ranklist.php?scope=y">Year</a>
+  </div>
+</div>
+<div class="container">
+  <table class="table table-hover table-striped">
+    <thead>
+      <tr class="toprow">
+        <th width="5%" align="center"><b><?php echo $MSG_Number?></b></th>
+        <th width="10%" align="center"><b><?php echo $MSG_USER?></b></th>
+        <th width="55%" align="center"><b><?php echo $MSG_NICK?></b></th>
+        <th width="10%" align="center"><b><?php echo $MSG_AC?></b></th>
+        <th width="10%" align="center"><b><?php echo $MSG_SUBMIT?></b></th>
+        <th width="10%" align="center"><b><?php echo $MSG_RATIO?></b></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+foreach($view_rank as $row){
+    echo "<tr>";
+  foreach($row as $table_cell){
+    echo "<td>";
+    echo "\t".$table_cell;
+    echo "</td>";
+  }
+  echo "</tr>";
+}
+?>
+      </tbody>
+  </table>
+  <div class="container text-center">
+<?php
+for($i = 0; $i <$view_total ; $i += $page_size) {
+  echo "<a href='./ranklist.php?start=" . strval ( $i ).($scope?"&scope=$scope":"") . "'>";
+  echo strval ( $i + 1 );
+  echo "-";
+  echo strval ( $i + $page_size );
+  echo "</a>&nbsp;";
+  if ($i % 250 == 200)
+    echo "<br>";
+}
+?>
+  </div>
 </div>
 
 <?php require_once("oj-footer.php");?>
