647922f54834f3a9c539a1af91d624c0ba499071
Mastojun <mastojun@gmail.com>
2015-10-18 22:17:19 +0900

    registerpage.php 수정

--- template/koreatech/registerpage.php
+++ template/koreatech/registerpage.php
@@ -1,56 +1,72 @@
 <!DOCTYPE html>
-<html>
+<html lang="ko">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <title><?php echo $view_title?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
-<div id="wrapper">
-	<?php require_once("oj-header.php");?>
-<div id=main>
-	<form action="register.php" method="post">
-	<br><br>
-	<center><table>
-		<tr><td colspan=2 height=40 width=500>&nbsp;&nbsp;&nbsp;<?php echo $MSG_REG_INFO?></td></tr>
-		<tr><td width=25%><?php echo $MSG_USER_ID?>:</td>
-			<td width=75%><input name="user_id" size=20 type=text>*</td>
-		</tr>
-		<tr><td><?php echo $MSG_NICK?>:</td>
-			<td><input name="nick" size=50 type=text></td>
-		</tr>
-		<tr><td><?php echo $MSG_PASSWORD?>:</td>
-			<td><input name="password" size=20 type=password>*</td>
-		</tr>
-		<tr><td><?php echo $MSG_REPEAT_PASSWORD?>:</td>
-			<td><input name="rptpassword" size=20 type=password>*</td>
-		</tr>
-		<tr><td><?php echo $MSG_SCHOOL?>:</td>
-			<td><input name="school" size=30 type=text></td>
-		</tr>
-		<tr><td><?php echo $MSG_EMAIL?>:</td>
-			<td><input name="email" size=30 type=text></td>
-		</tr>
-		<?php if($OJ_VCODE){?>
-		<tr><td><?php echo $MSG_VCODE?>:</td>
-			<td><input name="vcode" size=4 type=text><img alt="click to change" src="vcode.php" onclick="this.src='vcode.php#'+Math.random()">*</td>
-		</tr>
-		<?php }?>
-		<tr><td></td>
-			<td><input value="Submit" name="submit" type="submit">
-				&nbsp; &nbsp;
-				<input value="Reset" name="reset" type="reset"></td>
-		</tr>
-	</table></center>
-	<br><br>
-</form>
-
-<div id=foot>
-	<?php require_once("oj-footer.php");?>
-
-</div><!--end foot-->
-</div><!--end main-->
-</div><!--end wrapper-->
-	<?php require_once("include-bottom.php");?>
+<div class="container">
+  <?php require_once("oj-header.php");?>
+  <div class="col-sm-offset-2 col-sm-10">
+    <h3><?php echo $MSG_REG_INFO?></h3>
+  </div>
+  <form class="form-horizontal" action="register.php" method="post">
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_USER_ID?>*</label>
+      <div class="col-sm-10">
+        <input name="user_id" type="text" class="form-control" placeholder="input id">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_NICK?></label>
+      <div class="col-sm-10">
+        <input name="nick" type="text" class="form-control" placeholder="input nickname">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_PASSWORD?>*</label>
+      <div class="col-sm-10">
+        <input name="password" type="password" class="form-control" placeholder="input password">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_REPEAT_PASSWORD?>*</label>
+      <div class="col-sm-10">
+        <input name="rptpassword" type="password" class="form-control" placeholder="repeat password">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_SCHOOL?></label>
+      <div class="col-sm-10">
+        <input name="school" type="text" class="form-control" placeholder="school">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_EMAIL?></label>
+      <div class="col-sm-10">
+        <input name="email" type="text" class="form-control" placeholder="email">
+      </div>
+    </div>
+<?php if($OJ_VCODE){?>
+    <div class="form-group">
+      <label class="col-sm-2 control-label"><?php echo $MSG_VCODE?>*</label>
+      <div class="col-sm-8">
+        <input name="vcode" type="text" class="form-control" placeholder="vcode">
+      </div>
+      <div class="col-sm-2">
+        <img alt="click to change" src="vcode.php" onclick="this.src='vcode.php#'+Math.random()">
+      </div>
+    </div>
+<?php }?>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">가입</button>
+        <button type="reset" class="btn btn-default">초기화</button>
+      </div>
+    </div>
+  </form>
+<?php require_once("oj-footer.php");?>
+<?php require_once("include-bottom.php");?>
 </body>
 </html>
