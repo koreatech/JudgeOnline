a5630a47510b8a99e8a68c67b9a311c54f38d1e3
Mastojun <mastojun@gmail.com>
2015-10-25 20:33:32 +0900

    ko.php 추가 및 수정
    번역 안된 몇부분 수정, 들여쓰기 수정

---
+++ lang/ko.php
@@ -0,0 +1,121 @@
+<?php
+  //oj-header.php
+  $MSG_FAQ="자주 묻는 질문";
+  $MSG_BBS="게시판";
+  $MSG_HOME="처음 화면";
+  $MSG_PROBLEMS="문제";
+  $MSG_STATUS="채점 상황 열람";
+  $MSG_RANKLIST="순위";
+  $MSG_CONTEST="대회";
+  $MSG_RECENT_CONTEST="Recent";
+
+  $MSG_LOGOUT="LOGOUT";
+  $MSG_LOGIN="LOGIN";
+  $MSG_REGISTER="가입하기";
+  $MSG_ADMIN="관리자";
+  $MSG_STANDING="순위";
+  $MSG_STATISTICS="통계";
+  $MSG_USERINFO="정보 수정";
+  $MSG_MAIL="Mail";
+  //status.php
+  $MSG_Pending="대기";
+  $MSG_Pending_Rejudging="재체점 대기";
+  $MSG_Compiling="컴파일중";
+  $MSG_Running_Judging="채점중";
+  $MSG_Accepted="정답";
+  $MSG_Presentation_Error="출력 오류";
+  $MSG_Wrong_Answer="틀렸습니다";
+  $MSG_Time_Limit_Exceed="시간초과";
+  $MSG_Memory_Limit_Exceed="메모리 초과";
+  $MSG_Output_Limit_Exceed="출력이 너무 많음";
+  $MSG_Runtime_Error="실행 에러";
+  $MSG_TEST_RUN="테스트";
+  $MSG_Compile_Error="컴파일 에러";
+  $MSG_Compile_OK="컴파일 성공";
+  $MSG_RUNID="#";
+  $MSG_USER="사용자";
+  $MSG_PROBLEM="문제";
+  $MSG_RESULT="결과";
+  $MSG_MEMORY="메모리";
+  $MSG_TIME="시간";
+  $MSG_LANG="언어";
+  $MSG_CODE_LENGTH="길이";
+  $MSG_SUBMIT_TIME="시간";
+  //problemstatistics.php
+  $MSG_PD="PD";
+  $MSG_PR="PR";
+  $MSG_CI="CI";
+  $MSG_RJ="RJ";
+  $MSG_AC="AC";
+  $MSG_PE="PE";
+  $MSG_WA="WA";
+  $MSG_TLE="TLE";
+  $MSG_MLE="MLE";
+  $MSG_OLE="OLE";
+  $MSG_RE="RE";
+  $MSG_CE="CE";
+  $MSG_CO="CO";
+  $MSG_TR="Test";
+
+  //problemset.php
+  $MSG_SEARCH="Search";
+  $MSG_PROBLEM_ID="문제 ID";
+  $MSG_TITLE="문제명";
+  $MSG_SOURCE="출처";
+  $MSG_SUBMIT="제출";
+
+  //ranklist.php
+  $MSG_Number="순위";
+  $MSG_NICK="닉네임";
+  $MSG_SOLVED="해결 문제 수";
+  $MSG_RATIO="정답률";
+
+  //registerpage.php
+  $MSG_USER_ID="사용자 ID";
+  $MSG_PASSWORD="비밀번호";
+  $MSG_REPEAT_PASSWORD="비밀번호 반복 입력";
+  $MSG_SCHOOL="소속";
+  $MSG_EMAIL="이메일";
+  $MSG_REG_INFO="등록 정보";
+  $MSG_VCODE="Verify Code";
+
+  //problem.php
+  $MSG_NO_SUCH_PROBLEM="해당 문제를 열람할 수 없습니다!";
+  $MSG_Description="문제 설명"  ;
+  $MSG_Input="입력"  ;
+  $MSG_Output= "출력" ;
+  $MSG_Sample_Input= "입력 예시" ;
+  $MSG_Sample_Output= "출력 예시" ;
+  $MSG_HINT= "도움말" ;
+  $MSG_Source= "출처" ;
+  $MSG_Time_Limit="시간 제한";
+  $MSG_Memory_Limit="메모리 제한";
+
+  //admin menu
+  $MSG_SEEOJ="SeeOJ";
+  $MSG_ADD="Add";
+  $MSG_LIST="List";
+  $MSG_NEWS="News";
+  $MSG_TEAMGENERATOR="TeamGenerator";
+  $MSG_SETMESSAGE="SetMessage";
+  $MSG_SETPASSWORD="ChangePassWD";
+  $MSG_REJUDGE="Rejudge";
+  $MSG_PRIVILEGE="Privilege";
+  $MSG_GIVESOURCE="GiveSource";
+  $MSG_IMPORT="Import";
+  $MSG_EXPORT="Export";
+  $MSG_UPDATE_DATABASE="UpdateDatabase";
+  $MSG_ONLINE="Online";
+
+  //someone please translate these
+  $MSG_PRIVATE_WARNING="비공개 대회 입니다";
+  $MSG_WATCH_RANK="대회 순위를 보려면 여길 클릭하세요";
+  $MSG_Public="공개";
+  $MSG_Private="비공개";
+  $MSG_Running="진행중";
+  $MSG_Start="Start";
+  $MSG_TotalTime="총";
+  $MSG_LeftTime="남은시간";
+  $MSG_Ended="종료";
+?>
+
