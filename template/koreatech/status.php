580cb0b081c00bbd8940dd1687bc911c3225d884
Mastojun <mastojun@gmail.com>
2015-11-11 17:24:56 +0900

    status.php 의 ajax 정보를 text-mute 로 수정
    label은 별로고, warnning은 다른정보랑 색이 겹쳐서..

--- template/koreatech/status.php
+++ template/koreatech/status.php
@@ -150,8 +150,8 @@ xmlhttp.onreadystatechange=function()
     var row=findRow(solution_id);
     var r=xmlhttp.responseText;
     var ra=r.replace(/[\n\r]/g,"").split(",");
-    var loader="<img width=18 src=image/loader.gif>";
-    row.cells[3].innerHTML="<span class='label label-warning'>"+judge_result[ra[0]]+"</span>"+loader;
+    var loader=" <img width=18 src=image/loader.gif>";
+    row.cells[3].innerHTML="<span class='text-mute'>"+judge_result[ra[0]]+"</span>"+loader;
     row.cells[4].innerHTML=ra[1];
     row.cells[5].innerHTML=ra[2];
     if(ra[0]<4)
