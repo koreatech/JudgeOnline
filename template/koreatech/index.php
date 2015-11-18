cbe8b5fb5ef352dd7e741605da6884a4b4fd219c
Mastojun <mastojun@gmail.com>
2015-11-18 14:20:34 +0900

    index 의 jumbotron의 너비도 col-md-12 로
    너비를 news와 같도록 / mobile에서 좁은 폭으로 볼 때는 좌우여백이 없음

--- template/koreatech/index.php
+++ template/koreatech/index.php
@@ -8,7 +8,7 @@
   <?php require_once("oj-header.php");?>
   <?php echo $view_news?>
   <div class="container">
-    <div class="jumbotron">
+    <div class="col-md-12 jumbotron">
       <h1>Welcome!</h1>
       <p><a href="http://www.koreatech.ac.kr" target="_blank">한국기술교육대학교</a> <a href="http://cse.koreatech.ac.kr" target="_blank">컴퓨터공학부</a> Online Judge 에 오신것을 환영합니다.</p>
       <p>교내 프로그래밍 경시대회와, 학부생 대상의 Problem Solving교육을 위해 만들어졌습니다.</p>
