687421c912ea7cac4d4d5b0f0eb571ca3a5c31a5
Mastojun <mastojun@gmail.com>
2015-11-26 18:04:23 +0900

    index 내용 변경
    만들고있어서 진행중이라는 뉘앙스를 추가

--- template/koreatech/index.php
+++ template/koreatech/index.php
@@ -11,8 +11,8 @@
     <div class="col-md-12 jumbotron">
       <h1>Welcome!</h1>
       <p><a href="http://www.koreatech.ac.kr" target="_blank">한국기술교육대학교</a> <a href="http://cse.koreatech.ac.kr" target="_blank">컴퓨터공학부</a> Online Judge 에 오신것을 환영합니다.</p>
-      <p>교내 프로그래밍 경시대회와, 학부생 대상의 Problem Solving교육을 위해 만들어졌습니다.</p>
-      <p>조금씩 수정 중 입니다 :) 함께 개발 하실 분은 <a href="https://github.com/koreatech" target="_blank">github page</a>에 join하세요!</p>
+      <p>교내 프로그래밍 경시대회와, 학부생 대상의 Problem Solving교육을 위해 개선하고 있습니다.</p>
+      <p>함께 개발 하실 분은 <a href="https://github.com/koreatech" target="_blank">github page</a>에 join하세요!</p>
     </div>
   </div>
   <?php require_once("oj-footer.php");?>
