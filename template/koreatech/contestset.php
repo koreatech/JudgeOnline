a12ffcbb253e5f5624dee79e0f6f45d391f4b79a
Mastojun <mastojun@gmail.com>
2015-11-29 20:02:35 +0900

    contestset.php 정리
    doctype 수정, 코드 정리 등

--- template/koreatech/contestset.php
+++ template/koreatech/contestset.php
@@ -1,69 +1,65 @@
-<html>
+<!DOCTYPE html>
+<html lang="ko">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo $view_title?></title>
+  <title><?php echo $view_title?></title>
   <?php require_once("./template/".$OJ_TEMPLATE."/include-header.php");?>
 </head>
 <body>
 <?php
-  $navigation_tab = "contest";
-  require_once("oj-header.php");
+$navigation_tab = "contest";
+require_once("oj-header.php");
 ?>
 
 <div class="container">
-
 <div class="col-md-12 text-center">
-<h2>대회 목록<br><small>서버시간: <span id="nowdate"></span></small></h2>
+서버시간 : <span id="nowdate"></span>
 </div>
 <div class="col-md-12"><div class="table-responsive">
 <table class="table table-hover table-striped">
   <thead>
   <tr class=toprow align=center>
-    <th width=10%>ID</th>
-    <th width=50%>Name</th>
-    <th width=30%>Status</th>
-    <th width=10%>Private</th>
+    <th width=10%>#</th>
+    <th width=50%>대회 이름</th>
+    <th width=30%>상태</th>
+    <th width=10%>공개</th>
   </tr>
   </thead>
-	<tbody>
-			<?php 
-			foreach($view_contest as $row){
-				echo "<tr>";
-				foreach($row as $table_cell){
-					echo "<td>";
-					echo "\t".$table_cell;
-					echo "</td>";
-				}
-				echo "</tr>";
-			}
-			?>
-	</tbody>
+  <tbody>
+<?php 
+foreach($view_contest as $row){
+  echo "<tr>";
+  foreach($row as $table_cell){
+    echo "<td>";
+    echo "\t".$table_cell;
+    echo "</td>";
+  }
+  echo "</tr>";
+}
+?>
+  </tbody>
 </table></div></div>
 
-
-		
 <script>
-var diff=new Date("<?php echo date("Y/m/d H:i:s")?>").getTime()-new Date().getTime();
-//alert(diff);
-function clock()
-    {
-      var x,h,m,s,n,xingqi,y,mon,d;
-      var x = new Date(new Date().getTime()+diff);
-      y = x.getYear()+1900;
-      if (y>3000) y-=1900;
-      mon = x.getMonth()+1;
-      d = x.getDate();
-      xingqi = x.getDay();
-      h=x.getHours();
-      m=x.getMinutes();
-      s=x.getSeconds();
-  
-      n=y+"-"+mon+"-"+d+" "+(h>=10?h:"0"+h)+":"+(m>=10?m:"0"+m)+":"+(s>=10?s:"0"+s);
-      //alert(n);
-      document.getElementById('nowdate').innerHTML=n;
-      setTimeout("clock()",1000);
-    } 
-    clock();
+var diff = new Date("<?php echo date("Y/m/d H:i:s")?>").getTime() - new Date().getTime();
+var clock = function() {
+  var x,h,m,s,n,xingqi,y,mon,d;
+  var x = new Date(new Date().getTime() + diff);
+  y = x.getYear() + 1900;
+  if (y > 3000) {
+    y-=1900;
+  }
+  mon = x.getMonth()+1;
+  d = x.getDate();
+  xingqi = x.getDay();
+  h = x.getHours();
+  m = x.getMinutes();
+  s = x.getSeconds();
+
+  n=y+"-"+mon+"-"+d+" "+(h>=10?h:"0"+h)+":"+(m>=10?m:"0"+m)+":"+(s>=10?s:"0"+s);
+  document.getElementById('nowdate').innerHTML = n;
+  setTimeout("clock()",1000);
+};
+clock();
 </script>
 </div>
 <?php require_once("oj-footer.php");?>
